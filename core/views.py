3b9616b04e3de5c42385999d4a3c6ab9273c890c
Andy L <oleskainbox@gmail.com>
2026-07-15 15:22:22 -0400

    Views customization.

--- core/views.py
+++ core/views.py
@@ -1,6 +1,33 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+from django.contrib import messages
+from allauth.account.views import SignupView
+from allauth.account import app_settings
+
+class CustomSignupView(SignupView):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['custom_message'] = "Join 10,000+ visionary innovators using our cutting-edge, AI-powered platform!"
+        return context
+
+    def form_valid(self, form):
+        
+        response = super().form_valid(form)
+
+        if app_settings.EMAIL_VERIFICATION == app_settings.EmailVerificationMethod.MANDATORY:
+            messages.info(
+                self.request,
+                "We've sent you an email to verify your account. Please check your inbox."
+            )
+        else:
+            messages.success(
+                self.request,
+                f"Welcome {form.cleaned_data['first_name']}! Your account has been created."
+            )
+    
+        return response
+
 def index(request):
     return render(request, 'core/index.html')
 
